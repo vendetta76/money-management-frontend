7ed48541f1526b5124cbb2b89f0116d2fb966e3b
vendetta76 <diorvendetta76@gmail.com>
2025-05-07 03:26:31 +0700

    warm theme

--- src/hooks/useThemeAdvanced.ts
+++ src/hooks/useThemeAdvanced.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export type ThemeMode = "original" | "system" | "light" | "dark"
+export type ThemeMode = "system" | "light" | "dark" | "original" | "warm"
 
 export const useTheme = () => {
   const [theme, setTheme] = useState<ThemeMode>(() => {
@@ -10,13 +10,16 @@ export const useTheme = () => {
   useEffect(() => {
     const root = document.documentElement
 
+    // Reset class original & warm
+    root.classList.remove("original", "warm")
+
     if (theme === "system") {
       const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches
       root.classList.toggle("dark", prefersDark)
-      root.classList.toggle("original", false)
     } else {
       root.classList.toggle("dark", theme === "dark")
-      root.classList.toggle("original", theme === "original")
+      if (theme === "original") root.classList.add("original")
+      if (theme === "warm") root.classList.add("warm")
     }
 
     localStorage.setItem("theme", theme)
