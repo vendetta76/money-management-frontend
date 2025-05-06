e1ea2b8c3468a212d298b087bb58fbcbc49d3df6
vendetta76 <diorvendetta76@gmail.com>
2025-05-06 23:56:44 +0700

    dark mode fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { AuthProvider } from './context/AuthContext'
 import routes from './routes'
 import { Toaster } from 'react-hot-toast'
 import AutoLogoutWrapper from './components/AutoLogoutWrapper'
+import { useTheme } from './hooks/useThemeAdvanced' // ✅ Tambahkan
 
 function AppRoutes() {
   return useRoutes(routes)
 }
 
 function App() {
+  useTheme() // ✅ Trigger global theme (dark/original/system/light)
+
   return (
     <AuthProvider>
       <Router>
