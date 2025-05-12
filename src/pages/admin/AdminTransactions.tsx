40e2e2b1050f8481fc10c1e28b4736eb6c8c3f5d
vendetta76 <diorvendetta76@gmail.com>
2025-05-12 22:31:05 +0700

    ss

--- src/pages/admin/AdminTransactions.tsx
+++ src/pages/admin/AdminTransactions.tsx
@@ -1,10 +1,9 @@
-import AdminLayout from "/@/layouts/AdminLayout";
+import AdminLayout from "@/layouts/AdminLayout";
 
 export default function AdminTransactions() {
-    return (
-      <AdminLayout>
-     <div className="p-4 text-white">💰 Admin Transactions</div>;
-      </AdminLayout>
-      );
-  }
-  
+  return (
+    <AdminLayout>
+      <div className="p-4 text-white">💰 Admin Transactions</div>
+    </AdminLayout>
+  );
+}
