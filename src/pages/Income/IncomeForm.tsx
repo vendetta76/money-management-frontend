9447398b916f8a77c33bad0361c7bcadd0303735
vendetta76 <diorvendetta76@gmail.com>
2025-05-13 19:59:50 +0700

    ss

--- src/pages/Income/IncomeForm.tsx
+++ src/pages/Income/IncomeForm.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { db } from "../lib/firebaseClient";
 import {
   addDoc,
