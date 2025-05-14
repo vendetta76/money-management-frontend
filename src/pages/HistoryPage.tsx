f19c0bf83faeb130eead6df3a4f4b7a53ce9e838
vendetta76 <diorvendetta76@gmail.com>
2025-05-14 15:58:29 +0700

    ss

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -17,7 +17,7 @@ import {
   ChevronUp
 } from "lucide-react"
 import { usePageLockStatus } from "../hooks/usePageLockStatus"
-import PageLockAnnouncement from "../pages/admin/PageLockAnnouncement"
+import PageLockAnnouncement from "../components/admin/PageLockAnnouncement";
 
 interface EditEntry {
   description: string
