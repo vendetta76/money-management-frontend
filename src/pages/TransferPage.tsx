fb6f8a404a9743a3549b38c64a80e0b8794d23b1
vendetta76 <diorvendetta76@gmail.com>
2025-05-14 15:59:42 +0700

    ss

--- src/pages/TransferPage.tsx
+++ src/pages/TransferPage.tsx
@@ -16,7 +16,7 @@ import {
 import { useAuth } from "../context/AuthContext";
 import { toast } from "react-hot-toast";
 import { usePageLockStatus } from "../hooks/usePageLockStatus";
-import PageLockAnnouncement from "../pages/admin/PageLockAnnouncement";
+import PageLockAnnouncement from "../components/admin/PageLockAnnouncement";
 
 interface WalletEntry {
   id: string;
