3b75ec9a6c0e0efa7d0ed25179732e7691e077e6
vendetta76 <diorvendetta76@gmail.com>
2025-05-15 18:12:33 +0700

    ss

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,19 +1,29 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
-import en from "@/locales/en.json";
-import id from "@/locales/id.json";
+import en from '@/locales/clean_en.json';
+import id from '@/locales/clean_id.json';
+import es from '@/locales/clean_es.json';
+import zh from '@/locales/clean_zh.json';
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    id: { translation: id },
-  },
-  lng: "id", // default
-  fallbackLng: "id",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      id: { translation: id },
+      es: { translation: es },
+      zh: { translation: zh },
+    },
+    lng: 'en', // default
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage']
+    }
+  });
 
 export default i18n;
