bf164adeca5e50fa0e173d3abc9d547b700b6a80
vendetta76 <diorvendetta76@gmail.com>
2025-05-20 18:20:49 +0700

    Add PIN

--- src/components/PinEntryModal.tsx
+++ src/components/PinEntryModal.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Lock, AlertTriangle } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
-import { usePinTimeout } from '../../context/PinTimeoutContext';
+import { usePinTimeout } from '../context/PinTimeoutContext';
 
 type PinEntryModalProps = {
   onBack?: () => void;
