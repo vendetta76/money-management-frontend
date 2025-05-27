69bd1b66d6c02948dd5705b7bb26d21c771bfa18
vendetta76 <diorvendetta76@gmail.com>
2025-05-27 22:12:37 +0700

    dsd

--- src/pages/admin/AdminReports.tsx
+++
@@ -1,9 +0,0 @@
-import AdminLayout from "@/layouts/AdminLayout";
-
-export default function AdminReports() {
-    return (
-      <AdminLayout>
-        <div className="p-4 text-white"></div>
-      </AdminLayout>
-      );
-    }
--- src/pages/admin/AdminTransactions.tsx
+++
@@ -1,9 +0,0 @@
-import AdminLayout from "@/layouts/AdminLayout";
-
-export default function AdminTransactions() {
-  return (
-    <AdminLayout>
-      <div className="p-4 text-white">💰 Admin Transactions</div>
-    </AdminLayout>
-  );
-}
